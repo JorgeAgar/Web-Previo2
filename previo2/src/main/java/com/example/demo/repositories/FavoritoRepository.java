a87a965adfeb152e2e3edab5dfaa12ac74265510
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-05-05 11:00:13 -0500

    healthcontroller funcionando

--- previo2/src/main/java/com/example/demo/repositories/FavoritoRepository.java
+++ previo2/src/main/java/com/example/demo/repositories/FavoritoRepository.java
@@ -3,7 +3,8 @@ package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Favorito;
+import com.example.demo.entities.FavoritoId;
 
-public interface FavoritoRepository extends JpaRepository<Favorito, Integer> {
+public interface FavoritoRepository extends JpaRepository<Favorito, FavoritoId> {
 
 }
